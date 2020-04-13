0ddce8fa8e6b6e79e4ebd156487822de947f98ce
Cleiton <cleitonmattos87@gmail.com>
2020-04-13 20:50:17 -0300

    ARRAYS

---
+++ Array/array.js
@@ -0,0 +1,23 @@
+let aprovados = new Array ('Bia','Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Cleiton', 'Samanta', 'Sulivan']
+console.log(aprovados)
+
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados[3] = 'Natalia'
+aprovados.push('Nicolas')
+
+console.log(aprovados)
+aprovados.sort()
+console.log(aprovados.length)
+
+console.log(aprovados)
+
+aprovados = ['Cleiton', 'Samanta', 'Sulivan']
+
+aprovados.splice(1,2)
+console.log(aprovados)
