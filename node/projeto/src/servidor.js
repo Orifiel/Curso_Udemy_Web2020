8e2a2542b17be6006aa5cdd16f96d0b388fda107
Cleiton <cleitonmattos87@gmail.com>
2020-05-05 22:30:51 -0300

    middleware and api

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const porta = 3003
+
+const  app = express()
+
+
+app.use('/produtos', (req, res, next) =>{
+   res.send({ nome: 'Notebook', preco: 123.45 })
+})
+
+app.listen(porta, () => {
+   console.log(`Servidor esta executando na porta ${porta}`)
+})
