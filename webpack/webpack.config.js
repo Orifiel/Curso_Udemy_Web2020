27099d757a798df305a0a4761cf82c93f30fc45b
Orifiel <orifieleifiro@gmail.com>
2020-10-04 13:18:11 -0300

    meu primeiro webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development', //poodemos trocar o valor de mode para production, assim o arquivo gerado é minimizado
+    entry: './src/principal.js'
+}
