6bf13c9bd42b004a9d0531a64e30f945b43d62da
Orifiel <orifieleifiro@gmail.com>
2020-10-05 23:57:53 -0300

    Trabalhando com Webpack e usando Loaders

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,4 +1,7 @@
 import Pessoa from "./pesssoa"
+import './modulos/moduloA'
+import './assets'
+
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,28 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
-    mode: 'development', //poodemos trocar o valor de mode para production, assim o arquivo gerado é minimizado
-    entry: './src/principal.js'
+    mode: 'development', //podemos trocar o valor de mode para production, assim o arquivo gerado é minimizado
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css",
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', //add CSS a DOM injetando a tag <style>
+                'css-loader', //interpreta @import, url() ...
+                'sass-loader',
+            ]
+        }]
+    }
 }
